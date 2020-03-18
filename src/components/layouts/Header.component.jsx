c5d26263abd9667e5cac587d2690ceb3dd6f6670
Kayode-Kaleyemi <caresskakka@yahoo.com>
2020-03-18 18:18:35 +0100

    fixed navbar route

--- src/components/layouts/Header.component.jsx
+++ src/components/layouts/Header.component.jsx
@@ -5,27 +5,28 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
-import {useStyles } from "../../styles/lib/header.styles";
-
+import { useStyles } from "../../styles/lib/header.styles";
+import { NavLink } from 'react-router-dom';
+import navBar from '../../views/Navbar'
 
 const Header = () => {
-  const classes = useStyles();
-  
-  return (
-    <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            More
+    const classes = useStyles();
+
+    return (
+        <div className={classes.root}>
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography variant="h6" className={classes.title}>
+                        More
           </Typography>
-          <Button color="inherit">Get Started</Button>
-        </Toolbar>
-      </AppBar>
-    </div>
-  );
+                    <Button><NavLink to='/navbar'>Get Started</NavLink></Button>
+                </Toolbar>
+            </AppBar>
+        </div>
+    );
 };
 
 export default Header;
